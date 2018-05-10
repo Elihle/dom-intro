3c2ea34b020225be3d89b28c3e59ce0f59822309
Lihle <lihlemenz@gmail.com>
2018-05-10 10:22:33 +0200

    Seperated DOM from Logic into different files

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,27 +1,3 @@
-var calculateBtn = document.querySelector(".calculateBtn");
-var billTotal = document.querySelector(".billTotal");
-var billString = document.querySelector(".billString");
-
-
-// Dom function
-  function calculateBill(){
-    var totalData = billString.value;
-    var totals = totalPhoneBill(totalData);
-
-    billTotal.innerHTML = totals;
-    if (totals >= 20){
-      billTotal.classList.add("warning");
-    }
-    if (totals >= 30){
-      billTotal.classList.add("danger");
-    }
-    if (totals < 20){
-      billTotal.classList.remove("warning");
-    }
-    if (totals < 25){
-      billTotal.classList.remove("danger");
-    }
-  }
 
 //Logic function
 function totalPhoneBill(totalData){
@@ -40,5 +16,3 @@ function totalPhoneBill(totalData){
   }
   return total.toFixed(2);
 }
-
-calculateBtn.addEventListener('click', calculateBill);
